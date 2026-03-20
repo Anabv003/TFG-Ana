e91eab7dde6ec84544d878f4eec1ded10dba1f7b
Ana <Anabv003@gmail.com>
2026-03-20 11:23:44 +0100

    Se han añadido el control de las palas y corregido el problema con los rotores

--- extensions/ComandoAT/ComandoAT_python/extension.py
+++ extensions/ComandoAT/ComandoAT_python/extension.py
@@ -24,14 +24,11 @@ class AT_Comando(omni.ext.IExt):
         self.joints()
         self.build_ui()
 
-    def on_shutdown(self):
-        #self.stop_update()
-
-        self.on_physics_step_sub = None
-        self.on_stop_sub = None
-        self.plots_update_sub = None
+    # def on_shutdown(self):
+    #     pass
 
     def joints(self):
+
         self.articulations = Articulation(
             prim_paths_expr=["/root"]
         )
@@ -40,53 +37,54 @@ class AT_Comando(omni.ext.IExt):
         self.joint_names =list(self.articulations.joint_names)
         self.joint_name_to_index = {name: i for i, name in enumerate(self.joint_names)}
         self.rotor_joint_names = [name for name in self.joint_names if name.startswith("JRotor")]
-        self.palas_joint_names = [name for name in self.joint_names if name.startswith("JPalas")]
+        self.blade_joint_names = [name for name in self.joint_names if name.startswith("JPalas")]
 
         self.rotor_angles = {name : 0.0 for name in self.rotor_joint_names}
-        self.palas_vel = {name : 0.0 for name in self.palas_joint_names}
+        self.blade_speed = {name : 0.0 for name in self.blade_joint_names}
 
-
-    def on_physics_step(self, step_size:int):
-        self.current_time += step_size
-
-    def on_stop(self, event):
-        self.current_time = 0
-        # self.start_stop_tool_button.model.set_value(False)
-        # self.start_stop_update()
-        #self.stop_update()
+    # def on_timeline_play(self, event):
+    #     if not self.is_simulation_running:
+    #         self.joints()
+    #         self.is_simulation_running = True
+    
+    # def on_timeline_stop(self, event):
+        if self.is_simulation_running:
+            self.articulations = None 
+            self.is_simulation_running = False
 
     def init_vars(self):
-        self.event_stream = omni.kit.app.get_app_interface().get_message_bus_event_stream()
-        self.operator_uav_event = carb.events.type_from_string("NavSim.OperatorUAV")
-        
-        self.ext_utils = ExtensionUtils()
-        self.current_time = 0
-        self.stop_update_plot = True
+
+        self.is_simulation_running = False
 
         self.articulations = None
         self.joint_names = []
         self.joint_name_to_index = {}
 
         self.rotor_joint_names = []
-        self.palas_joint_names = []
+        self.blade_joint_names = []
 
         self.rotor_angles = {}
-        self.palas_vel = {}
+        self.blade_speed = {}
 
         self.rotor_sliders = {}
-        self.palas_sliders = {}
+        self.blase_sliders = {}
 
         self.current_joint_positions = None
-        
-        self.physx_interface = omni.physx.get_physx_interface()
 
-        self.timeline = omni.timeline.get_timeline_interface()
-        timeline_stream = self.timeline.get_timeline_event_stream()
-        self.on_stop_sub = timeline_stream.create_subscription_to_pop_by_type(
-            int(omni.timeline.TimelineEventType.STOP), 
-            self.on_stop
-        )
-#Interfaz
+        #  # Timeline callbacks
+        # self.timeline = omni.timeline.get_timeline_interface()
+        # timeline_stream = self.timeline.get_timeline_event_stream()
+
+        # self.on_stop_sub = timeline_stream.create_subscription_to_pop_by_type(
+        #     int(omni.timeline.TimelineEventType.STOP), 
+        #     self.on_timeline_stop
+        # )
+
+        # self.on_play_sub = timeline_stream.create_subscription_to_pop_by_type(
+        #     int(omni.timeline.TimelineEventType.PLAY), 
+        #     self.on_timeline_play
+        # )
+
     def build_ui(self):
         # The ui.RasterPolicy.NEVER is to always update plots line drawing
         self.window = ui.Window(
@@ -136,6 +134,39 @@ class AT_Comando(omni.ext.IExt):
 
                             ui.Spacer(height=15)
 
+                    #Poner cada pala a una velocidad diferente
+                    self.blade_speeds = ui.CollapsableFrame(
+                        title="Blade Speed", 
+                        collapsed=False
+                    )
+
+                    with self.blade_speeds:
+                        with ui.VStack(style={"margin": 1}, height=0, spacing=5):
+                            for blade_name in self.blade_speed.keys():
+                                with ui.HStack(alignment=ui.Alignment.RIGHT):
+                                    ui.Label(blade_name)
+
+                                    slider = ui.FloatSlider(
+                                        min=-120, 
+                                        max=120,
+                                        step=1,
+                                        precision=1,
+                                        style={
+                                            "background_color": ui.color(0.13),
+                                            "secondary_color": ui.color(0.3),
+                                            "draw_mode": ui.SliderDrawMode.FILLED
+                                        }
+                                    )
+
+                                    slider.model.set_value(0.0)
+                                    slider.model.add_value_changed_fn(
+                                        lambda model,
+                                        name=blade_name: self.on_blade_change(model, name)
+                                    )
+
+                                    self.blase_sliders[blade_name] = slider
+
+
     def on_rotor_change(self, model, rotor_name):
         value = model.get_value_as_float()
         # Guardar valor
@@ -148,7 +179,34 @@ class AT_Comando(omni.ext.IExt):
 
         angle_rad = np.deg2rad(angle_deg)
 
-        self.articulations.set_joint_positions(
+        # self.articulations.set_joint_positions(
+        #     positions=np.array([angle_rad]),
+        #     joint_indices=np.array([idx])
+        # )
+        self.articulations.set_joint_position_targets(
             positions=np.array([angle_rad]),
             joint_indices=np.array([idx])
         )
+
+    def on_blade_change(self, model, blade_name):
+        value = model.get_value_as_float()
+        # Guardar valor
+        self.blade_speed[blade_name] = value
+        # Aplicar al joint
+        self.apply_blade_vel(blade_name, value)
+
+    def apply_blade_vel(self, blade_name, vel):
+        idx = self.articulations.get_joint_index(blade_name)
+
+        #angle_rad = np.deg2rad(angle_deg)
+
+        self.articulations.set_joint_velocity_targets(
+            velocities =np.array([vel]),
+            joint_indices=np.array([idx])
+        )
+        # self.articulations.set_joint_position_targets(
+        #     positions=np.array([1.57,1.57,1.57,1.57,1.57,1.57]),
+        #     joint_indices=range(6, 12)
+        # )
+
+
